4907542906f842aab7318cbbf8068998b26ee676
Gavin <gavin.loch@gmail.com>
2023-06-08 21:06:52 -0500

    Messing with test lib

--- tests/main.c
+++ tests/main.c
@@ -4,7 +4,51 @@ void test(int a, int b) {
     munit_assert_int(a, ==, b);
 }
 
-int main() {
-    test(1,0);
-    return 0;
+MunitResult my_test(const MunitParameter params[], void *user_data) {
+    // silence compiler warning
+    (void) params;
+    (void) user_data;
+
+    munit_assert(0 != 1);
+    
+    munit_assert_false(0);
+
+    /* Calling Errors
+    munit_error("FAIL");
+    munit_errorf("Goodbye, cruel %s", "world");
+    */
+
+    const unsigned char val_uchar = 'b';
+    munit_assert_uchar(val_uchar, ==, 'b');
+
+    const char *stewardesses = "stewardesses";
+    munit_assert_string_equal(stewardesses, "stewardesses");
+
+    return MUNIT_OK;
+}
+
+static MunitTest test_suite_tests[] = {
+    {
+        (char*) "/example/compare",
+        my_test,
+        NULL,
+        NULL,
+        0,
+        NULL
+    }
+};
+
+static const MunitSuite test_suite = {
+    (char*) "",
+    test_suite_tests,
+    NULL,
+    0,
+    MUNIT_SUITE_OPTION_NONE
+};
+
+
+#include <stdlib.h>
+
+int main(int argc, char *argv[MUNIT_ARRAY_PARAM(argc + 1)]) {
+    return munit_suite_main(&test_suite, NULL, argc, argv);
 }
